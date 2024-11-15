bdd49195a7adcb9b4fc64b8921b52a17941968c4
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2024-11-15 09:45:40 +0100

    DONE: Exercice 9

---
+++ Exercice-9/script.js
@@ -0,0 +1,18 @@
+// const number = parseInt(prompt("Veuillez saisir un nombre : "));
+
+let number;
+
+while(Number.isInteger(number) === false ){
+    number = parseInt(prompt("Veuillez saisir un nombre : "));
+}
+
+let resultat = "";
+
+for (let i = 1; i <= 10; i += 1) {
+  resultat += number + " X "+ i + " = " + number * i + "\n";
+}
+
+alert(resultat)
+
+
+
