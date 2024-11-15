c100271dbfe53cd7a0759bb46237f52a2277dc6f
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2024-11-15 09:20:47 +0100

    DONE: Exercice 6

---
+++ Exercice-6/resteDivision.js
@@ -0,0 +1,5 @@
+function resteDivision(firstNumber, secondNumber){
+    return firstNumber % secondNumber;
+}
+
+export { resteDivision };
---
+++ Exercice-6/script.js
@@ -0,0 +1,10 @@
+import { resteDivision } from "./resteDivision.js";
+
+
+// une variable qui va utiliser la fonction resteDivision pour avoir le résultat
+
+
+let resultat = resteDivision(5, 2);
+
+
+console.log(resultat);
